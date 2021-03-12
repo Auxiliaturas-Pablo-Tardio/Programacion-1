81061167fe72c1d5ea174f4fc7b37447c7172211
pablotardio <pablo.tv30@gmail.com>
2021-03-11 20:29:16 -0400

    sumamatriz y contar unicos de matriz con y sin rango final

--- 2-2020/Matriz/PMatriz.dfm
+++ 2-2020/Matriz/PMatriz.dfm
@@ -2,23 +2,24 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 320
-  ClientWidth = 570
+  ClientHeight = 389
+  ClientWidth = 705
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
+  Menu = MainMenu1
   OldCreateOrder = False
   OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
   object StringGrid1: TStringGrid
-    Left = 120
-    Top = 64
-    Width = 320
-    Height = 120
+    Left = 43
+    Top = 80
+    Width = 230
+    Height = 145
     TabOrder = 0
     RowHeights = (
       24
@@ -28,8 +29,8 @@ object Form1: TForm1
       24)
   end
   object Cargar: TButton
-    Left = 104
-    Top = 248
+    Left = 88
+    Top = 264
     Width = 75
     Height = 25
     Caption = 'Cargar'
@@ -37,12 +38,56 @@ object Form1: TForm1
     OnClick = CargarClick
   end
   object VerDescargar: TButton
-    Left = 288
-    Top = 248
+    Left = 185
+    Top = 264
     Width = 75
     Height = 25
     Caption = 'VerDescargar'
     TabOrder = 2
     OnClick = VerDescargarClick
+  end
+  object StringGrid2: TStringGrid
+    Left = 336
+    Top = 80
+    Width = 225
+    Height = 136
+    TabOrder = 3
+  end
+  object Cargar2: TButton
+    Left = 368
+    Top = 256
+    Width = 75
+    Height = 25
+    Caption = 'Cargar2'
+    TabOrder = 4
+    OnClick = Cargar2Click
+  end
+  object Descargar: TButton
+    Left = 486
+    Top = 256
+    Width = 75
+    Height = 25
+    Caption = 'Descargar'
+    TabOrder = 5
+    OnClick = DescargarClick
+  end
+  object MainMenu1: TMainMenu
+    Left = 16
+    Top = 24
+    object Ejercicios1: TMenuItem
+      Caption = 'Ejercicios'
+      object ContarUnicos1: TMenuItem
+        Caption = 'Contar Unicos'
+        OnClick = ContarUnicos1Click
+      end
+      object ContarUnicosParam1: TMenuItem
+        Caption = 'Contar Unicos Param'
+        OnClick = ContarUnicosParam1Click
+      end
+      object SumarMatricesEnla11: TMenuItem
+        Caption = 'Sumar Matrices En la 1'
+        OnClick = SumarMatricesEnla11Click
+      end
+    end
   end
 end
--- 2-2020/Matriz/UCMatriz.pas
+++ 2-2020/Matriz/UCMatriz.pas
@@ -20,6 +20,11 @@ Type
           procedure addFilas(nroFilas:Word);
           procedure addColumnas(nroColumnas:Word);
           function getElemento(fila,columna:Word):Real;
+          function contarUnicos():word;
+           function contarUnicosParamFin(ff,cf:Word):word;
+          function esUnico(eleVerif:Real): boolean;
+          function esUnicoParam(ff,cf:word;eleVerif:Real):boolean;
+          procedure sumarAMiMatriz(otraMatriz:Matriz);
       End;
 
 implementation
@@ -34,6 +39,100 @@ end;
 procedure Matriz.addFilas(nroFilas: Word);
 begin
       Filas:=Filas+nroFilas ;
+end;
+
+function Matriz.contarUnicos: word;
+var i,j,c:word ;
+begin
+c:=0;
+   for i := 1 to filas do
+   begin
+        for j := 1 to Cols do
+           begin
+                // estamos listos para sacar el elemento actual
+                if (esUnico(Elemento[j,i])) then
+                begin
+                  c:=c+1;
+                end;
+           end;
+   end;
+
+   Result:=c;
+
+end;
+
+function Matriz.contarUnicosParamFin(ff, cf: Word): word;
+var i,j,c:word ;
+begin
+c:=0;
+if((ff<=filas) and(cf<=cols)) then
+begin
+   for i := 1 to ff do
+   begin
+        for j := 1 to cf do
+           begin
+                // estamos listos para sacar el elemento actual
+                if (esUnicoParam(ff,cf,Elemento[j,i])) then
+                begin
+                  c:=c+1;
+                end;
+           end;
+   end;
+
+   Result:=c;
+end
+else
+  //Devolver excepcion
+   Result:=0;
+end;
+
+function Matriz.esUnico(eleVerif: Real): boolean;
+var
+  i,j,c: Integer;
+  b:Boolean ;
+begin
+  c:=0;
+  b:=true;
+     for i := 1 to filas do
+      begin
+         for j := 1 to Cols do
+            if(elemento[j,i]=eleVerif)then
+              c:=c+1;
+      end;
+     if(c=1)then
+     begin
+     b:=true;
+     end
+     else
+     b:=false ;
+
+    Result:= b;
+end;
+
+function Matriz.esUnicoParam(ff, cf: word; eleVerif: Real): boolean;
+
+
+var
+  i,j,c: Integer;
+  b:Boolean ;
+begin
+  c:=0;
+  b:=true;
+     for i := 1 to ff do
+      begin
+         for j := 1 to cf do
+            if(elemento[j,i]=eleVerif)then
+              c:=c+1;
+      end;
+     if(c=1)then
+     begin
+     b:=true;
+     end
+     else
+     b:=false ;
+
+    Result:= b;
+
 end;
 
 function Matriz.getCols: Word;
@@ -63,5 +162,25 @@ if ((fila<=Filas) and (columna<=Cols))  then
       Elemento[columna,fila]:=elem;
 end;
 
+
+procedure Matriz.sumarAMiMatriz(otraMatriz: Matriz);
+var
+  i: Integer;
+  j: Integer;
+begin
+if( (filas=otraMatriz.getFilas()) and (Cols=otraMatriz.getCols())) then
+begin
+  for i := 1 to filas do
+      begin
+        for j := 1 to Cols do
+        begin
+              //aqui ya tenemos el elemento actual
+               Elemento[j,i]:=Elemento[j,i]+otraMatriz.getElemento(i,j);
+        end;
+      end;
+end
+else
+// Levantar una excepcion
+end;
 
 end.
